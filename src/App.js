d99463264d751be708d97aa9829e8764ca4cab8b
Yassa122 <yassa.ashraf@yahoo.com>
2023-09-30 13:33:26 +0300

    my projects text

--- src/App.js
+++ src/App.js
@@ -2,43 +2,46 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import TextComponent from './components/TextComponent';
-import Contact from './components/Contact'; // Import the Contact component
 import logo from './logo/Group 1190.svg';
+import MyProjects from './components/myProjects';
 
 function App() {
   return (
-    <div 
-      style={{ 
-        minHeight: '100vh', 
-        background: 'linear-gradient(to bottom right, #070547, #020004, #1B033A)' 
-      }} 
-      className="App relative" 
-    > 
+    <div
+      style={{
+        minHeight: '180vh',
+        background: 'linear-gradient(to bottom right, #070547, #020004, #1B033A)',
+      }}
+      className="App relative"
+    >
       {/* Logo */}
-      <img 
-        src={logo} 
-        alt="Logo" 
+      <img
+        src={logo}
+        alt="Logo"
         style={{
-          position: 'absolute', 
-          right: '0', 
-          top: '-2000', 
-          width: '877.995px', 
-          height: '595.704px', 
+          position: 'absolute',
+          right: '0',
+          top: '-2000',
+          width: '877.995px',
+          height: '595.704px',
           flexShrink: 0,
           maxWidth: '65%',
           maxHeight: '100%',
-
-          minHeight: '100%',
-          
-          zIndex: 1 // Set z-index to 1 for the logo
-        }} 
+          zIndex: 1, // Set z-index to 1 for the logo
+        }}
       />
 
       <div className="flex flex-col h-screen z-10 relative">
         <Navbar />
-        <div className="flex-1 flex flex-col items-center justify-center"> {/* Use flex-col here */}
-          <TextComponent text1="Developing intelligent, seamless "  text2=" software."  subText="Innovation leaps from student's code."/>
-          
+        <div className="flex-1 flex flex-col items-center justify-center">
+          {/* Use flex-col here */}
+          <TextComponent
+            text1="Developing intelligent, seamless "
+            text2=" software."
+            subText="Innovation leaps from student's code."
+          />
+          {/* Added "My Projects" text here */}
+          <MyProjects/>
         </div>
       </div>
     </div>
---
+++ src/components/myProjects.js
@@ -0,0 +1,14 @@
+// src/components/MyProjects.js
+import React from 'react';
+import './styles/MyProjects.css'; // Importing the CSS file
+
+const MyProjects = () => {
+    return (
+      <div className="text-center mt-12 flex justify-center space-x-4 lg:mr-[800px] mr-[70px]">
+        <p className="my-text">My</p> 
+        <p className="projects-text">Projects</p>
+      </div>
+    );
+  };
+  
+  export default MyProjects;
