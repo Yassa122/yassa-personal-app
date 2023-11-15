42c0790a02abe6e99f8315b7165c1fae1a91b17b
Yassa122 <yassa.ashraf@yahoo.com>
2023-11-15 16:35:59 +0200

    added lottie react pkg for animations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About'; // Import the About component
+// ... other imports (e.g., Navbar, if you have one)
 
 function App() {
- 
-
   return (
-    
-    <div>
-    
-    <Home/>
-    </div>
+    <Router>
+      {/* If you have a Navbar, include it here so it's always visible */}
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Define other routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
