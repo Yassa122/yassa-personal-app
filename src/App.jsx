d6ee633f14c9dbad122aeba7d48e0a8187dd56b9
Yassa122 <yassa.ashraf@yahoo.com>
2023-11-15 19:11:37 +0200

    renamed

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About'; // Import the About component
+// ... other imports (e.g., Navbar, if you have one)
+
+function App() {
+  return (
+    <Router>
+      {/* If you have a Navbar, include it here so it's always visible */}
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        {/* Define other routes as needed */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
