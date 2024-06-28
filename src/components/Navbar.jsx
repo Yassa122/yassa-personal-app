c1009653d758d0d84397d4805e065a8686b36857
Yassa122 <yassa.ashraf@yahoo.com>
2024-06-28 21:04:02 +0300

    refactor: Update Navbar component styling and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React from "react";
+import "./styles/navbar.css"; // Import the CSS file
+import { Link } from "react-router-dom";
+import logo from "./logo/favicon.ico"; // Import your logo
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4 fixed w-full top-0 z-50">
-      <div className="container mx-auto flex justify-between items-center">
-        <a href="#" className="text-white font-bold text-xl">Kesho's Portfolio</a>
-        <div className="space-x-4">
-          <a href="#home" className="text-gray-300 hover:text-white">Home</a>
-          <a href="#projects" className="text-gray-300 hover:text-white">Projects</a>
-          <a href="#contact" className="text-gray-300 hover:text-white">Contact</a>
+    <nav className="text-white p-4 sticky top-0 z-30 w-full backdrop-blur-md">
+      <div className="container mx-auto lg:px-16 xl:px-32">
+        <div className="flex justify-between">
+          <div className="text-2xl">
+            {/* Replace text with logo */}
+            <Link to="/" className="transition-colors duration-500 ease-in-out">
+              <img src={logo} alt="Logo" className="h-8 w-8" />{" "}
+              {/* Adjust size as needed */}
+            </Link>
+          </div>
+          <div>
+            <ul className="flex space-x-4">
+              <li>
+                <Link
+                  to="/"
+                  className="transition-colors duration-500 ease-in-out"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Projects"
+                  className="transition-colors duration-500 ease-in-out"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/About"
+                  className="transition-colors duration-500 ease-in-out"
+                >
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
   );
-}
-
+};
+//
 export default Navbar;
