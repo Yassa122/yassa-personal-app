d1c8f02de5d9ae11c26a824b5574ad19822403e7
Yassa122 <Yassa122@users.noreply.github.com>
2025-01-31 03:55:13 +0200

    feat: Add route for Bachelor Thesis details in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About"; // Import the About component
 import Projects from "./components/Projects";
 // ... other imports (e.g., Navbar, if you have one)
-
+import BachelorThesisDetails from "./components/Bachelor-thesis";
 function App() {
   return (
     <Router>
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Projects" element={<Projects />} />
+        <Route path="/bachelor-thesis" element={<BachelorThesisDetails />} />
 
         {/* Define other routes as needed */}
       </Routes>
